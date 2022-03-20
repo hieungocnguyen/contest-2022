9271b748a2ade47da31bed7d6a5a38571ca31809
Nguyen Ngoc Hieu <ngnohieu@gmail.com>
2022-03-20 15:11:22 +0700

    upload post

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const dark = document.querySelector(".dark");
 const slider = document.querySelector(".button-switch");
 const switchMode = document.querySelector(".switch-mode");
 const reactposts = document.querySelectorAll(".newsfeed_post_react");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+const inputTextArea = document.querySelector(".post_input");
+const buttonPost = document.querySelector(".button_post");
+const contentPost = document.querySelector(".content");
+const targetInsert = document.querySelector(".target_insert");
 
 switchMode.addEventListener("click", function () {
   slider.classList.toggle("apply_action");
@@ -17,4 +23,29 @@ reactposts.forEach(function (react) {
   react.addEventListener("click", function () {
     react.classList.toggle("reacted");
   });
+});
+
+contentPost.addEventListener("click", function () {
+  modal.classList.toggle("active_model");
+  inputTextArea.value = "";
+  buttonPost.classList.add("disable");
+});
+
+close.addEventListener("click", function () {
+  modal.classList.toggle("active_model");
+});
+
+inputTextArea.addEventListener("input", function () {
+  if (inputTextArea.value === "") {
+    buttonPost.classList.add("disable");
+  } else {
+    buttonPost.classList.remove("disable");
+  }
+});
+
+buttonPost.addEventListener("click", function () {
+  if (inputTextArea.value !== "") {
+    targetInsert.innerHTML = inputTextArea.value;
+    modal.classList.toggle("active_model");
+  }
 });
